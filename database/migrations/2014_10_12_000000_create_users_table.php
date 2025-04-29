0fee8f8793272a8c5ecf2386325df7b5f5ecf4fe
ZeragonAnachronised <ioanidiilya@gmail.com>
2025-04-29 21:31:10 +0600

    migrations done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Query\Expression;
 
 return new class extends Migration
 {
@@ -13,12 +14,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 32);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->decimal('budget', 10, 2, true)->default(0);
+            $table->dateTimeTz('last_online')->nullable();
+            $table->boolean('premium_user')->default(false);
+            $table->uuid('user_tag');
+            $table->enum('status', ['student', 'teacher', 'worker'])->nullable();
+            $table->set('destiny', ['learning', 'qualification', 'development'])->nullable();
             $table->string('password');
+            $table->dateTime('last_event')->useCurrentOnUpdate();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
